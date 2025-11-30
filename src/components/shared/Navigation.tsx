e508af03b28ae48f5df97f365b2458873aaff7e5
ibrahim benyahia <khalilbenyahia2@gmai.com>
2025-11-30 17:39:53 +0100

    final

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -7,7 +7,7 @@ import { FaBrain } from "react-icons/fa";
 
 const Naviagation = ()=>{
     return (
-        <Box opacity={.2} _hover={{opacity:1}} transition={"opacity 300ms linear"} display={"flex"} justifyContent={{base:"center", lg:"auto"}} gap={4} flexDir={{base:"row", lg:"column"}} pos={"fixed"} left={{base:"50%", lg:4}} translate={{base:"-50%", lg:"auto"}}  top={{base:"auto", lg:4}} bottom={{base:4, lg:"auto"}} p={4} rounded={"full"} shadow={"md"} translateY={"-50%"}  bg={"bg.200"} w={{base:"80%", lg:"auto"}}  zIndex={100}>
+        <Box opacity={.7} _hover={{opacity:1}} transition={"opacity 300ms linear"} display={"flex"} justifyContent={{base:"center", lg:"auto"}} gap={4} flexDir={{base:"row", lg:"column"}} pos={"fixed"} left={{base:"50%", lg:4}} translate={{base:"-50%", lg:"auto"}}  top={{base:"auto", lg:4}} bottom={{base:4, lg:"auto"}} p={4} rounded={"full"} shadow={"md"} translateY={"-50%"}  bg={"bg.200"} w={{base:"80%", lg:"auto"}}  zIndex={100}>
             <Tooltip content="Go To Learn Page">
                 <Button asChild rounded={"full"} bgColor={"accent.400"} _hover={{bgColor:"accent.600"}} transition={"background-color 300ms linear"} p={4}>
                 <Link href="/"><IoHomeOutline/></Link>
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,8 +1,18 @@
+import Box from "@/components/motion/Box";
+import Heading from "@/components/motion/Heading";
+import Text from "@/components/motion/Text";
+import {useRouteError} from "react-router-dom";
 const Error = ()=>{
+    
+    const error:any = useRouteError();
     return (
-        <>
-            Error
-        </>
+        
+        <Box bg={"accent.200"} w={"full"} h={"100dvh"} display={"flex"} alignItems={"center"} justifyContent={"center"} flexDirection={"column"} gap={6}>
+
+            <Heading textAlign={"center"} size={{base:"4xl", lg:"5xl"}} color={"red.600"}>Oops</Heading>
+            <Text color={"GrayText"}>{`${error.status} | ${error.statusText}`}</Text>
+
+        </Box>
     )
 }
 
